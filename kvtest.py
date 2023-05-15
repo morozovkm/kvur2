37b0a626fcc0711f4037ffb923592611a2e30686
morozovkm <morozovkm@yandex.ru>
2023-05-15 20:00:20 +0300

    Добавил j,hf,jnre bcrk.xtybq

--- kvtest.py
+++ kvtest.py
@@ -1,3 +1,13 @@
-a = int(input('Введите a: '))
-b = int(input('Введите b: '))
-c = int(input('Введите c: '))
+baddata = True
+while baddata == True:
+    try:
+        a = int(input('Введите a: '))
+        b = int(input('Введите b: '))
+        c = int(input('Введите c: '))
+        baddata = False
+    except:
+        print('Не удалось получить данные!')
+
+
+
+
