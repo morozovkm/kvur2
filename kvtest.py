e3a12d8563a84b70f59a66b7477ee1a4d8d20fbc
morozovkm <morozovkm@yandex.ru>
2023-05-15 20:37:26 +0300

    Revert "Добавил обработку исключений"
    This reverts commit 1714691d

--- kvtest.py
+++ kvtest.py
@@ -1,27 +1,3 @@
-import math
-
-baddata = True
-while baddata == True:
-    try:
-        a = int(input('Введите a: '))
-        b = int(input('Введите b: '))
-        c = int(input('Введите c: '))
-        baddata = False
-    except:
-        print('Не удалось получить данные!')
-
-D = (b * b) - (4 * a * c)
-
-if D > 0:
-    d = math.sqrt(D)
-    X1 = ((-b) + d) / (2*a)
-    X2 = ((-b) - d) / (2 * a)
-    print(f'Уравнение имеет два корня X1 = {X1}, X2 = {X2}.')
-
-elif D == 0:
-    X1 = (-b) / (2 * a)
-    print(f'Уравнение имеет один корень X1 = {X1}.')
-else:
-    print('Уравнение не имеет корней')
-
-
+a = int(input('Введите a: '))
+b = int(input('Введите b: '))
+c = int(input('Введите c: '))
