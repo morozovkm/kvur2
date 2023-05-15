ff8403220e6b2b047a124763765ef77ec80fef6c
morozovkm <morozovkm@yandex.ru>
2023-05-15 20:00:56 +0300

    Revert "Добавил j,hf,jnre bcrk.xtybq"
    This reverts commit 37b0a626fcc0711f4037ffb923592611a2e30686.

--- kvtest.py
+++ kvtest.py
@@ -1,13 +1,3 @@
-baddata = True
-while baddata == True:
-    try:
-        a = int(input('Введите a: '))
-        b = int(input('Введите b: '))
-        c = int(input('Введите c: '))
-        baddata = False
-    except:
-        print('Не удалось получить данные!')
-
-
-
-
+a = int(input('Введите a: '))
+b = int(input('Введите b: '))
+c = int(input('Введите c: '))
