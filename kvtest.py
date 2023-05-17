4606736d6630961a45c844b08a5a4762d3501f24
alkaz07 <alkaz07@yandex.ru>
2023-05-17 18:35:49 +0300

    Исправил помарки в коде

--- kvtest.py
+++ kvtest.py
@@ -1,7 +1,7 @@
 from math import sqrt
 
 baddata = True
-while baddata == True:
+while baddata:
     try:
         a = int(input('Введите a: '))
         b = int(input('Введите b: '))
@@ -14,7 +14,7 @@ D = (b * b) - (4 * a * c)
 
 if D > 0:
     d = sqrt(D)
-    X1 = ((-b) + d) / (2*a)
+    X1 = ((-b) + d) / (2 * a)
     X2 = ((-b) - d) / (2 * a)
     print(f'Уравнение имеет два корня X1 = {X1}, X2 = {X2}.')
 
@@ -23,5 +23,3 @@ elif D == 0:
     print(f'Уравнение имеет один корень X1 = {X1}.')
 else:
     print('Уравнение не имеет корней')
-
-
